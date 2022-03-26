8d2222abb52f7d4eb4bcd47a9ec39f9a10946cf9
Favour <toluwaniokunowo@gmail.com>
2022-03-26 13:49:06 +0100

    add starter files

---
+++ stories_seed.js
@@ -0,0 +1,32 @@
+window.feed = (function() {
+	const stories = [
+		{
+			id: 1,
+			title: 'Jany',
+			text:
+				's that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here making it look like readable En',
+			votes: 0,
+			postedAt: '3 mins ago',
+			storyImage: 'https://loremflickr.com/320/240?random=1'
+		},
+		{
+			id: 2,
+			title: 'Seni',
+			text:
+				'Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through',
+			votes: 0,
+			postedAt: '10 mins ago',
+			storyImage: 'https://loremflickr.com/320/240?random=2'
+		},
+		{
+			id: 3,
+			title: 'Samy',
+			text:
+				'snt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this th',
+			votes: 0,
+			postedAt: '32 mins ago',
+			storyImage: 'https://loremflickr.com/320/240?random=3'
+		}
+	];
+	return { stories: stories };
+})();
