d8d90d5bc91d039737364b65789cc7e58dbf3290
Favour <toluwaniokunowo@gmail.com>
2022-03-26 15:23:14 +0100

    understand data binding in vue

--- stories_seed.js
+++ stories_seed.js
@@ -7,7 +7,8 @@ window.Feed = (function () {
                 's that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here making it look like readable En',
             votes: 0,
             postedAt: '3 mins ago',
-            storyImage: 'https://loremflickr.com/320/240?random=1'
+            storyImage: 'https://loremflickr.com/320/240?random=1',
+            url: '#'
         },
         {
             id: 2,
@@ -16,7 +17,8 @@ window.Feed = (function () {
                 'Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through',
             votes: 0,
             postedAt: '10 mins ago',
-            storyImage: 'https://loremflickr.com/320/240?random=2'
+            storyImage: 'https://loremflickr.com/320/240?random=2',
+            url: '#'
         },
         {
             id: 3,
@@ -25,7 +27,8 @@ window.Feed = (function () {
                 'snt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this th',
             votes: 0,
             postedAt: '32 mins ago',
-            storyImage: 'https://loremflickr.com/320/240?random=3'
+            storyImage: 'https://loremflickr.com/320/240?random=3',
+            url: '#'
         }
     ];
     return { stories: stories };
