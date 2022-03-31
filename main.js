7f69e4efd37145b30c54ab2362b477e8d6df5490
Favour <toluwaniokunowo@gmail.com>
2022-03-31 17:40:32 +0100

    sort up-voted stories

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ Vue.createApp({
             stories: Feed.stories
         }
     },
+
+    computed: {
+        sortedStories() {
+            return this.stories.sort((a, b) => b.votes - a.votes);
+        }
+    },
+
     methods: {
         upVote(storyId) {
             const index = this.stories.findIndex(story => story.id === storyId)
