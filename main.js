e8cc27e54abc4a4e73dc517a22ca689b31c87306
Favour <toluwaniokunowo@gmail.com>
2022-03-31 17:28:22 +0100

    add up-vote function

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ Vue.createApp({
         return {
             stories: Feed.stories
         }
-    }
+    },
+    methods: {
+        upVote(storyId) {
+            const index = this.stories.findIndex(story => story.id === storyId)
+            this.stories[index].votes++
+        }
+    },
 }).mount('#app')
